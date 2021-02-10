c67946d323bc334c4a854b4de5fad7d2af835637
David Maier <david.maier@atempo.at>
2021-02-10 13:14:28 +0100

    My first action is ready

---
+++ entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# initialize arguments
+OPTIND=1 
+db=""
+workdir=$(PWD)
+
+while getopts "d:w:" opt; do
+    case "$opt" in
+    d) db=${OPTARG//[[:blank:]]/}
+       ;;
+    w) workdir=${OPTARG//[[:blank:]]/}
+       ;;
+    *) exit 1
+    esac
+done
+
+shift $((OPTIND - 1))
+
+[ "${1:-}" = "--" ] && shift
+
+if [[ -z "$db" ]]; then
+    echo  "Argument -t (template database) missing"
+    exit 1
+fi
+
+echo "$db" > .env
+
+cd "$workdir" || exit 1
+
+prisma migrate deploy --preview-feature
